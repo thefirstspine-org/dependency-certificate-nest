a5e849cfce0e3c667b9a14bdac88788b3174995e
thefirstspine <teddy@coretizone.com>
2020-06-08 18:25:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+
+# 📦 dependency-certificate-nest // @thefirstspine/certificate-nest
+
+Built on top of <https://github.com/thefirstspine/certificate-authority|@thefirstspine/certificate-authority>. Provides Nest services & guard.
+
+## Install
+
+```bash
+npm i @thefirstspine/certificate-nest@latest
+```
+
+## Configuration
+
+The configuration is the same as <https://github.com/thefirstspine/certificate-authority|@thefirstspine/certificate-authority> module.
+
+## Documentation
+
+### CertificateGuard
+
+Guard to filter incoming requests with the @thefirstspine/certificate-authority validator.
+
+## How to use
+
+### Build
+
+```bash
+npm run build
+```
+
+### Lint
+
+```bash
+npm run lint
+```
+
+### Publish on NPM
+
+```bash
+npm publish
+```
+
+## License
+
+TFS Platform is NOT licensed. You are free to download, view, run the repository. You are NOT allowed to redistribute this project for both commercial and non-commercial use. Deal with it.
---
+++ src/certificate.guard.ts
@@ -0,0 +1,60 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ValidatorService } from '@thefirstspine/certificate-authority';
+import { LogsService } from '@thefirstspine/logs-nest';
+
+/**
+ * 
+ */
+@Injectable()
+export class CertificateGuard implements CanActivate {
+
+  constructor(private readonly logsService: LogsService) {}
+
+  /**
+   * @inheritdoc
+   * @param context
+   */
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> {
+    if (
+      !context.switchToHttp().getRequest().headers ||
+      !context.switchToHttp().getRequest().headers['x-client-cert']
+    ) {
+      return false;
+    }
+
+    // Get encoding
+    const supportedEncoding = {
+      'base64': this.decodeBase64.bind(this),
+    };
+    const encoding: string = context.switchToHttp().getResponse().headers['x-client-cert-encoding'] ?
+      context.switchToHttp().getResponse().headers['x-client-cert-encoding'] :
+      supportedEncoding[0];
+    if (!supportedEncoding[encoding]) {
+      this.logsService.error(`Unsupported x-client-cert-encoding: ${encoding}`);
+      return false;
+    }
+
+    // Get public key
+    const certificate: string = supportedEncoding[encoding](context.switchToHttp().getRequest().headers['x-client-cert']);
+
+    // Validate
+    const validatorService: ValidatorService = new ValidatorService();
+    if (!validatorService.challenge(certificate)) {
+      this.logsService.error(`Cannot challenge the public certificate with private key`);
+      return false;
+    }
+
+    return true; // ok, next!
+  }
+
+  /**
+   * Decode base64 input
+   * @param input
+   */
+  protected decodeBase64(input: string): string {
+    return Buffer.from(input, 'base64').toString();
+  }
+
+}
